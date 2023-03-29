7a620595d79c36cadfc81dd54f0cd17d0dbd3e6f
Sorana L <alexandra.badarau@yahoo.com>
2023-03-29 12:40:04 +0300

    completare tema curs 10

--- src/main/java/curs10/Exercitiu2.java
+++ src/main/java/curs10/Exercitiu2.java
@@ -27,7 +27,7 @@ public class Exercitiu2 extends BaseTest {
 	public void checkBook2 () {
 		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
 		comingSoonCategory.get(1).click();
-		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
+		WebElement theForestBook = driver.findElement(By.cssSelector("a[href=\"the-forest\"]"));
 		assertTrue(theForestBook.isDisplayed());
 	}
 		
@@ -43,43 +43,43 @@ public class Exercitiu2 extends BaseTest {
 	public void checkBook4 () {
 		List<WebElement> awardWinnersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
 		awardWinnersCategory.get(3).click();
-		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
+		WebElement theForestBook = driver.findElement(By.cssSelector("div[id=\"sc_tab_1456822448754_7\"] a[href*=\"forest\"]"));		
 		assertTrue(theForestBook.isDisplayed());
 		theForestBook.click();	
 		assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
 	}
 		
 	
-	@Test
-	public void checkBook() {
-		
-		List<WebElement> newReleasesCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
-		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
-		List<WebElement> bestSellersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
-		List<WebElement> awardWinnersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-		
-		
-		List<String> list = new ArrayList<String>();
-		list.add(newReleasesCategory);
-		list.add(comingSoonCategory);
-		list.add(bestSellersCategory);
-		list.add(awardWinnersCategory);
-		
-		
-		int i;
-		for (i=0; i<=list.size(); i++) {
-			
-			newReleasesCategory.get(0).click();
-			comingSoonCategory.get(1).click();
-			bestSellersCategory.get(2).click();
-			awardWinnersCategory.get(3).click();
-			WebElement theForestBook = driver.findElement(By.linkText("The forest"));
-			assertTrue(theForestBook.isDisplayed());
-			theForestBook.click();	
-			assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
-			
-		}
-		
-	}	
+//	@Test
+//	public void checkBook() {
+//		
+//		List<WebElement> newReleasesCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
+//		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
+//		List<WebElement> bestSellersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
+//		List<WebElement> awardWinnersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
+//		
+//		
+//		List<String> list = new ArrayList<String>();
+//		list.add(newReleasesCategory);
+//		list.add(comingSoonCategory);
+//		list.add(bestSellersCategory);
+//		list.add(awardWinnersCategory);
+//		
+//		
+//		int i;
+//		for (i=0; i<=list.size(); i++) {
+//			
+//			newReleasesCategory.get(0).click();
+//			comingSoonCategory.get(1).click();
+//			bestSellersCategory.get(2).click();
+//			awardWinnersCategory.get(3).click();
+//			WebElement theForestBook = driver.findElement(By.linkText("The forest"));
+//			assertTrue(theForestBook.isDisplayed());
+//			theForestBook.click();	
+//			assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
+//			
+//		}
+//		
+//	}	
 	
 }
