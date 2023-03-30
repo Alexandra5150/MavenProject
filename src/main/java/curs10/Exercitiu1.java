47a7bf00f880a5a9911fac4f29abb3d827c5d26e
Sorana L <alexandra.badarau@yahoo.com>
2023-03-30 13:34:27 +0300

    completare tema curs 10

--- src/main/java/curs10/Exercitiu1.java
+++ src/main/java/curs10/Exercitiu1.java
@@ -5,21 +5,35 @@ import static org.testng.Assert.assertTrue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
 import utils.BaseTest;
 
 public class Exercitiu1 extends BaseTest {
 
 	@Test
 	public void displayElements() {
-	WebElement login = driver.findElement(By.linkText("Login"));
-	assertTrue(login.isDisplayed());
-	login.click();
 	
-	WebElement username = driver.findElement(By.id("log"));
-	assertTrue(username.isDisplayed());
-	
-	WebElement password = driver.findElement(By.id("password"));
-	assertTrue(username.isDisplayed());
+    //verificam daca elementul e vizibil inainte de click pe login
+    WebElement logingemail = driver.findElement(By.id("log"));
+
+    //verificam daca elementul e vizibil inainte de click pe login
+    WebElement password = driver.findElement(By.id("password"));
+
+    SoftAssert sa = new SoftAssert();
+    sa.assertFalse(logingemail.isDisplayed());
+    sa.assertFalse(password.isDisplayed());
+
+    //accesam login
+    WebElement login = driver.findElement(By.className("menu_user_login"));
+    login.click();
+
+    //verificam daca elementul e vizibil dupa click pe login
+    sa.assertTrue(logingemail.isDisplayed());
+
+    //verificam daca elementul e vizibil dupa click pe login
+    sa.assertTrue(password.isDisplayed());
+    sa.assertAll();
 	
 	}
 	
--- src/main/java/curs10/Exercitiu2.java
+++ src/main/java/curs10/Exercitiu2.java
@@ -15,71 +15,55 @@ import java.util.ArrayList;
 
 public class Exercitiu2 extends BaseTest {
 
-	@Test(priority = 1)
-	public void checkBook1 () {		
-		List<WebElement> newReleasesCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-		newReleasesCategory.get(0).click();		
-		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
-		assertTrue(theForestBook.isDisplayed());		
-	}
-	
-	@Test(priority = 2)
-	public void checkBook2 () {
-		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-		comingSoonCategory.get(1).click();
-		WebElement theForestBook = driver.findElement(By.cssSelector("a[href=\"the-forest\"]"));
-		assertTrue(theForestBook.isDisplayed());
-	}
-		
-	@Test(priority = 3)
-	public void checkBook3 () {
-		List<WebElement> bestSellersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-		bestSellersCategory.get(2).click();
-		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
-		assertTrue(theForestBook.isDisplayed());
-	}
-	
-	@Test(priority = 4)
-	public void checkBook4 () {
-		List<WebElement> awardWinnersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-		awardWinnersCategory.get(3).click();
-		WebElement theForestBook = driver.findElement(By.cssSelector("div[id=\"sc_tab_1456822448754_7\"] a[href*=\"forest\"]"));		
-		assertTrue(theForestBook.isDisplayed());
-		theForestBook.click();	
-		assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
-	}
-		
-	
-//	@Test
-//	public void checkBook() {
+//	@Test(priority = 1)
+//	public void checkBook1 () {		
+//		List<WebElement> newReleasesCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
+//		newReleasesCategory.get(0).click();		
+//		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
+//		assertTrue(theForestBook.isDisplayed());		
+//	}
+//	
+//	@Test(priority = 2)
+//	public void checkBook2 () {
+//		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
+//		comingSoonCategory.get(1).click();
+//		WebElement theForestBook = driver.findElement(By.cssSelector("a[href=\"the-forest\"]"));
+//		assertTrue(theForestBook.isDisplayed());
+//	}
 //		
-//		List<WebElement> newReleasesCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
-//		List<WebElement> comingSoonCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
-//		List<WebElement> bestSellersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));		
+//	@Test(priority = 3)
+//	public void checkBook3 () {
+//		List<WebElement> bestSellersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
+//		bestSellersCategory.get(2).click();
+//		WebElement theForestBook = driver.findElement(By.linkText("The forest"));
+//		assertTrue(theForestBook.isDisplayed());
+//	}
+//	
+//	@Test(priority = 4)
+//	public void checkBook4 () {
 //		List<WebElement> awardWinnersCategory = driver.findElements(By.cssSelector("li[class*=\"sc_tabs_title\"]"));
-//		
-//		
-//		List<String> list = new ArrayList<String>();
-//		list.add(newReleasesCategory);
-//		list.add(comingSoonCategory);
-//		list.add(bestSellersCategory);
-//		list.add(awardWinnersCategory);
-//		
-//		
-//		int i;
-//		for (i=0; i<=list.size(); i++) {
-//			
-//			newReleasesCategory.get(0).click();
-//			comingSoonCategory.get(1).click();
-//			bestSellersCategory.get(2).click();
-//			awardWinnersCategory.get(3).click();
-//			WebElement theForestBook = driver.findElement(By.linkText("The forest"));
-//			assertTrue(theForestBook.isDisplayed());
-//			theForestBook.click();	
-//			assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
-//			
-//		}
-//		
-//	}	
+//		awardWinnersCategory.get(3).click();
+//		WebElement theForestBook = driver.findElement(By.cssSelector("div[id=\"sc_tab_1456822448754_7\"] a[href*=\"forest\"]"));		
+//		assertTrue(theForestBook.isDisplayed());
+//		theForestBook.click();	
+//		assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/");
+//	}
+		
+    @Test 
+    public void checkBook() throws InterruptedException {
+        
+          List<WebElement> theForestBook = driver.findElements(By.className("sc_tabs_title"));
+           
+            for (WebElement book : theForestBook) {
+                    book.click();
+                    assertTrue(driver.findElement(By.linkText("The forest")).isDisplayed());       
+          }
+     
+            driver.findElement(By.linkText("The forest")).click();
+            assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/shop/the-forest/");
+
+            Thread.sleep(3000);
+    }
+
 	
 }
